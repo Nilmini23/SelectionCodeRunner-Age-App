9efe733b8d0513d58dff2f6bf0aaaeb26a3cb3ac
Kanchana Nilmini Pusweli Mulla Gamage <nilminipuswella@gmail.com>
2023-09-15 18:44:43 +0300

    Initial commit

---
+++ src/AgeApp.java
@@ -0,0 +1,21 @@
+
+public class AgeApp{
+
+	public static void main(String[] args) {
+		// Make an app which tells you what you can do depending your age.
+
+			int age = 20;
+			if (age > 0 && age < 18)
+			{
+				System.out.println("You are under age.");
+			}
+			else
+			{
+				System.out.println("You are an adult.");
+			}
+			{
+				System.out.println("Press space to exit");
+			}
+	}
+
+}
---
+++ src/Testing1.java
@@ -0,0 +1,43 @@
+
+public class Testing1 {
+
+	public static void main(String[] args) {
+
+		 int age = 100;
+		 
+		 if (age > 0 && age < 18)
+		 {
+			 System.out.println("You are under age.");
+			 if (age >= 15)
+			 {
+				 System.out.println("You can drive a moped.");
+			 }
+		 }
+		 else if (age >= 65)
+		 {
+			 System.out.println("You are retired.");
+		 }
+		 else
+			 System.out.println("you are an adult.");
+		 if (age == 18)
+		 {
+			 System.out.println("you can drive a car.");
+		 }
+		 if (age==10 || age==20 || age==30 || age==40 || age==50 || age==60 || age==70 || age==80 || age==90 || age==100 || age==110 || age==120 )
+		 {
+			 System.out.println("Anniversary Party!"); 
+		 }
+		 if (age == 100)
+		 {
+			 System.out.println("Congratulations!");
+			 System.out.println("Congratulations!");
+			 System.out.println("Congratulations!");
+		 }
+		 if (age < 50 && age > 40)
+		 {
+			 System.out.println("Happy mid life!");
+ 
+		 }
+	}
+
+}
